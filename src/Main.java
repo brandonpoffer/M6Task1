1c7f1cceff595e3d81c8d83857e831fc82dc8f52
brandonpoffer <poffbr@mail.uc.edu>
2025-02-28 10:53:33 -0500

    final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+
+        double c = 0.0;
+        double f;
+        String trash;
+        boolean done = false;
+        do {
+            System.out.println("Please enter temperature in Celsius: ");
+            if(in.hasNextDouble()) {
+                c = in.nextDouble();
+                f = (c * 1.8) + 35;
+                System.out.println("The temperature " + c + " in Celsius is " + f + " in Fahrenheit.");
+                done = true;
+
+            }
+            else {
+                trash = in.nextLine();
+                System.out.println("Invalid input: " + trash);
+                System.out.println("Please enter a valid temperature!");
+            }
+        } while(!done);
+
     }
 }
